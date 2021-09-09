059b149e420640f6db5cbb83d6a1e5c20431f275
tenzap <fabstz-it@yahoo.fr>
2021-09-09 09:52:36 +0200

    pick PR: https://github.com/k9mail/k-9/pull/3936

--- app/core/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
+++ app/core/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
@@ -20,6 +20,7 @@ public class MessageViewInfo {
     public final boolean isSubjectEncrypted;
     public final AttachmentResolver attachmentResolver;
     public final String text;
+    public final String textPlainFormatted;
     public final CryptoResultAnnotation cryptoResultAnnotation;
     public final List<AttachmentViewInfo> attachments;
     public final String extraText;
@@ -29,7 +30,7 @@ public class MessageViewInfo {
     public MessageViewInfo(
             Message message, boolean isMessageIncomplete, Part rootPart,
             String subject, boolean isSubjectEncrypted,
-            String text, List<AttachmentViewInfo> attachments,
+            String text, String textPlainFormatted, List<AttachmentViewInfo> attachments,
             CryptoResultAnnotation cryptoResultAnnotation,
             AttachmentResolver attachmentResolver,
             String extraText, List<AttachmentViewInfo> extraAttachments) {
@@ -39,6 +40,7 @@ public class MessageViewInfo {
         this.subject = subject;
         this.isSubjectEncrypted = isSubjectEncrypted;
         this.text = text;
+        this.textPlainFormatted = textPlainFormatted;
         this.cryptoResultAnnotation = cryptoResultAnnotation;
         this.attachmentResolver = attachmentResolver;
         this.attachments = attachments;
@@ -47,9 +49,11 @@ public class MessageViewInfo {
     }
 
     static MessageViewInfo createWithExtractedContent(Message message, Part rootPart, boolean isMessageIncomplete,
-            String text, List<AttachmentViewInfo> attachments, AttachmentResolver attachmentResolver) {
+            String text, String textPlainFormatted, List<AttachmentViewInfo> attachments,
+            AttachmentResolver attachmentResolver) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart, null, false, text, attachments, null, attachmentResolver, null,
+                message, isMessageIncomplete, rootPart, null, false, text, textPlainFormatted, attachments, null,
+                attachmentResolver, null,
                 Collections.<AttachmentViewInfo>emptyList());
     }
 
@@ -59,20 +63,21 @@ public class MessageViewInfo {
             Part emptyPart = new MimeBodyPart(emptyBody, "text/plain");
             String subject = message.getSubject();
             return new MessageViewInfo(message, isMessageIncomplete, emptyPart, subject, false, null, null, null, null,
-                    null, null);
+                    null, null, null);
         } catch (MessagingException e) {
             throw new AssertionError(e);
         }
     }
 
     public static MessageViewInfo createForMetadataOnly(Message message, boolean isMessageIncomplete) {
-        return new MessageViewInfo(message, isMessageIncomplete, null, null, false, null, null, null, null, null, null);
+        return new MessageViewInfo(message, isMessageIncomplete, null, null, false, null, null, null, null, null, null, null);
     }
 
     MessageViewInfo withCryptoData(CryptoResultAnnotation rootPartAnnotation, String extraViewableText,
             List<AttachmentViewInfo> extraAttachmentInfos) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, attachments,
+                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, textPlainFormatted,
+                attachments,
                 rootPartAnnotation,
                 attachmentResolver,
                 extraViewableText, extraAttachmentInfos
@@ -81,7 +86,8 @@ public class MessageViewInfo {
 
     MessageViewInfo withSubject(String subject, boolean isSubjectEncrypted) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, attachments,
+                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, textPlainFormatted,
+                attachments,
                 cryptoResultAnnotation, attachmentResolver, extraText, extraAttachments
         );
     }
